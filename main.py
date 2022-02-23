7f5cd604ecb1fae9ffbc9caa6741ca2fedc007a9
Abdel Ghani Muhra <67690813+AbdelMuhra@users.noreply.github.com>
2022-02-23 04:48:07 +0000

    part 1 complete: the program generates two cards for each player (the user and the computer). and displays the score of each one.

--- main.py
+++ main.py
@@ -12,3 +12,19 @@
 ## The cards in the list have equal probability of being drawn.
 ## Cards are not removed from the deck as they are drawn.
 ## The computer is the dealer.
+
+from art import logo
+import random
+from replit import clear
+
+cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+players = {}
+if input("Do you want to play a game of Blackjack? ('y'|'n')? ") == 'y':
+  clear()
+  print(logo)
+  user_cards = [random.choice(cards),random.choice(cards)]
+  user_score = sum(user_cards)
+  computer_cards = [random.choice(cards),random.choice(cards)]
+  computer_score = sum(computer_cards)
+  print(f"your cards are: {user_cards}. current_score: {user_score}")
+  print(f"computer cards are: {computer_cards}. current_score: {computer_score}")
