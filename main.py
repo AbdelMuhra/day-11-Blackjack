c151826c7dcfa995bfc29fb93b59c0dd04edf9a8
Abdel Ghani Muhra <67690813+AbdelMuhra@users.noreply.github.com>
2022-02-24 04:21:08 +0000

    game keeps going to someone wins by blackjack or over 21.

--- main.py
+++ main.py
@@ -18,13 +18,45 @@ import random
 from replit import clear
 
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-players = {}
-if input("Do you want to play a game of Blackjack? ('y'|'n')? ") == 'y':
-  clear()
-  print(logo)
-  user_cards = [random.choice(cards),random.choice(cards)]
-  user_score = sum(user_cards)
-  computer_cards = [random.choice(cards),random.choice(cards)]
-  computer_score = sum(computer_cards)
-  print(f"your cards are: {user_cards}. current score: {user_score}")
-  print(f"computer's first card is: {computer_cards[0]}")
+
+
+#make a loop that keeps the program going, here are the conditions:
+#1. any player achieves a combo with a sum of 21 GAME OVER, winner with Blackjack!
+#2. player has a hand with a sum over 21, GAME OVER, other player wins
+#3. ELSE keep the loop going!!!!
+user_score = 0
+computer_score = 0
+game_on = True
+while game_on:
+  if user_score == 21:
+    game_on = False
+    print(f"You Won by BlackJack😎")
+  elif computer_score == 21:
+    game_on = False
+    print(f"You Lost, computer won by BlackJack")
+  elif user_score > 21:
+    game_on = False
+    print(f"You Lost, you went over.")
+  elif computer_score > 21:
+    game_on = False
+    print(f"You Won, computer went over.")
+  else:
+    if input("Do you want to play a game of Blackjack? ('y'|'n')? ") == 'y':
+      clear()
+      print(logo)
+      user_cards = [random.choice(cards),random.choice(cards)]
+      user_score = sum(user_cards)
+      computer_cards = [random.choice(cards),random.choice(cards)]
+      computer_score = sum(computer_cards)
+      print(f"your cards are: {user_cards}. current score: {user_score}")
+      print(f"computer's first card is: {computer_cards[0]}")
+      if input("Type 'y' to get another card, type 'n' to pass:") == 'y':
+        user_cards.append(random.choice(cards))
+        user_score = sum(user_cards)
+      elif computer_score < 18:
+        computer_cards.append(random.choice(cards))
+        computer_score = sum(computer_cards)
+      print(f"your cards are: {user_cards}. current score: {user_score}")
+      print(f"computer's first card is: {computer_cards[0]}")
+
+
