45cbb94f2347e44165b7ec0203398f7110096e2a
Abdel Ghani Muhra <67690813+AbdelMuhra@users.noreply.github.com>
2022-02-24 04:52:16 +0000

    should be complete now :)

--- main.py
+++ main.py
@@ -1,32 +1,14 @@
-############### Blackjack Project #####################
-
-
-############### Our Blackjack House Rules #####################
-
-## The deck is unlimited in size. 
-## There are no jokers. 
-## The Jack/Queen/King all count as 10.
-## The the Ace can count as 11 or 1.
-## Use the following list as the deck of cards:
-## cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-## The cards in the list have equal probability of being drawn.
-## Cards are not removed from the deck as they are drawn.
-## The computer is the dealer.
-
+###Blackjack Project ###
 from art import logo
 import random
 from replit import clear
 
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
-
-#make a loop that keeps the program going, here are the conditions:
-#1. any player achieves a combo with a sum of 21 GAME OVER, winner with Blackjack!
-#2. player has a hand with a sum over 21, GAME OVER, other player wins
-#3. ELSE keep the loop going!!!!
 user_score = 0
 computer_score = 0
 game_on = True
+
 while game_on:
   if user_score == 21:
     game_on = False
